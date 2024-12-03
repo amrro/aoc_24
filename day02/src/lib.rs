f48a44d65981bf95645dd862f380e12db2dcf331
amrro <git@amrro.xyz>
2024-12-03 10:17:56 +0200

    Day 02: part 02 solution 💩

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -8,30 +8,6 @@ use std::{
 const SAMPLE: &str = "src/sample.txt";
 const INPUT: &str = "src/input.txt";
 
-#[derive(Debug, PartialEq)]
-enum Trend {
-    Ascending,
-    Descending,
-}
-
-impl Trend {
-    fn determine_trend(report: &[u8]) -> Self {
-        if report[0] < report[1] {
-            Self::Ascending
-        } else {
-            Self::Descending
-        }
-    }
-
-    /// First Safety Check: The levels are either all increasing or all decreasing.
-    fn matches(&self, first_level: u8, second_level: u8) -> bool {
-        match self {
-            Trend::Ascending => first_level < second_level,
-            Trend::Descending => first_level > second_level,
-        }
-    }
-}
-
 pub fn read_file(path: &str) -> io::BufReader<fs::File> {
     let file_path = Path::new(&path);
     let file = fs::File::open(file_path)
@@ -40,39 +16,66 @@ pub fn read_file(path: &str) -> io::BufReader<fs::File> {
     io::BufReader::new(file)
 }
 
-pub fn read_reports() -> Vec<std::vec::Vec<u8>> {
+pub fn read_reports() -> Vec<std::vec::Vec<i32>> {
     read_file(INPUT)
         .lines()
         .map_while(Result::ok)
         .map(|line| {
             line.split_whitespace()
-                .map(|level| level.parse::<u8>().unwrap())
-                .collect::<Vec<u8>>()
+                .map(|level| level.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>()
         })
-        .collect::<Vec<Vec<u8>>>()
+        .inspect(|r| println!("{:?}", r))
+        .collect::<Vec<Vec<i32>>>()
 }
 
-/// Second Safety Check: Any two adjacent levels differ by at least 1 and at most 3
-#[inline]
-pub fn is_levels_safe(first_level: u8, second_level: u8) -> bool {
-    (1..=3).contains(&first_level.abs_diff(second_level))
+pub fn check_safety(report: &[i32]) -> bool {
+    let mut diffs = vec![];
+    for window in report.windows(2) {
+        diffs.push(window[1] - window[0]);
+    }
+
+    // Safety Check:
+    let valid_diffs = diffs.iter().all(|&d| d.abs() > 0 && d.abs() < 4);
+    if !valid_diffs {
+        return false;
+    }
+
+    let is_all_positives = diffs.iter().all(|&d| d > 0);
+    let is_all_negatives = diffs.iter().all(|&d| d < 0);
+
+    is_all_positives || is_all_negatives
 }
 
-pub fn is_report_safe(report: &[u8]) -> bool {
-    let trend = Trend::determine_trend(report);
-    report.windows(2).all(|pair| {
-        if let [first, second] = pair {
-            trend.matches(*first, *second) && is_levels_safe(*first, *second)
-        } else {
-            false
+pub fn dampen_check_safety(report: &[i32]) -> bool {
+    // if the report itself is safe, no need to check.
+    if check_safety(report) {
+        return true;
+    }
+
+    for idx in 0..report.len() {
+        let mut new_report = report.to_vec();
+        new_report.remove(idx);
+
+        if check_safety(&new_report) {
+            return true;
         }
-    })
+    }
+
+    false
 }
 
 pub fn part_one() -> usize {
     read_reports()
         .iter()
-        .filter(|&report| is_report_safe(report))
+        .filter(|&report| check_safety(report))
+        .count()
+}
+
+pub fn part_two() -> usize {
+    read_reports()
+        .iter()
+        .filter(|&report| dampen_check_safety(report))
         .count()
 }
 
@@ -81,9 +84,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_read_reports() {
+    fn test_part_one() {
         let safe_reports = part_one();
 
         assert_eq!(safe_reports, 287);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let safe_reports = part_two();
+        assert_eq!(safe_reports, 354);
     }
 }
